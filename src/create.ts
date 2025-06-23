df8a5b7512eccfb9e52e587854f632629da41d4e
tariqul420 <tariqul.developer@gmail.com>
2025-06-23 17:21:01 +0600

    feat: update Prisma schema and migration files for User, Movie, and Review models

--- src/create.ts
+++ src/create.ts
@@ -3,55 +3,69 @@ import { PrismaClient } from '../generated/prisma';
 const prisma = new PrismaClient();
 
 async function main() {
-  const createUser = await prisma.user.create({
+  // const createUser = await prisma.user.create({
+  //   data: {
+  //     name: 'Tariqul Islam 2',
+  //     email: 'tariqul8.dev8@gmail.com',
+  //     password: '123456',
+  //   },
+  // });
+  // console.log(createUser, 'Create user response');
+
+  // const createMovies = await prisma.movie.createMany({
+  //   data: [
+  //     {
+  //       title: 'Inception',
+  //       description: 'A thief who steals corporate secrets through the use of dream-sharing technology.',
+  //       releaseDate: new Date('2010-07-16'),
+  //       director: 'Christopher Nolan',
+  //       genre: 'Sci-Fi',
+  //     },
+  //     {
+  //       title: 'The Godfather',
+  //       description: 'The aging patriarch of an organized crime dynasty transfers control to his reluctant son.',
+  //       releaseDate: new Date('1972-03-24'),
+  //       director: 'Francis Ford Coppola',
+  //       genre: 'Crime',
+  //     },
+  //     {
+  //       title: 'Spirited Away',
+  //       releaseDate: new Date('2001-07-20'),
+  //       director: 'Hayao Miyazaki',
+  //       genre: 'Fantasy',
+  //     },
+  //     {
+  //       title: 'Parasite',
+  //       description: 'Greed and class discrimination threaten the newly formed symbiotic relationship.',
+  //       releaseDate: new Date('2019-05-30'),
+  //       director: 'Bong Joon-ho',
+  //       genre: 'Thriller',
+  //     },
+  //     {
+  //       title: 'Interstellar',
+  //       description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  //       releaseDate: new Date('2014-11-07'),
+  //       director: 'Christopher Nolan',
+  //       genre: 'Sci-Fi',
+  //     },
+  //   ],
+  // });
+  // console.log(createMovies, 'Create user response');
+
+  const newUser = await prisma.user.create({
     data: {
-      name: 'Tariqul Islam2',
-      email: 'tariqul.dev8@gmail.com',
+      name: 'Shakib',
+      email: 'shakib@example.com',
       password: '123456',
+      watchlist: {
+        connect: [{ id: '01JYE7NJQ459PFEF9DAZAK9H6D' }],
+      },
+    },
+    include: {
+      watchlist: true,
     },
   });
-  console.log(createUser, 'Create user response');
-
-  const createMovies = await prisma.movie.createMany({
-    data: [
-      {
-        title: 'Inception',
-        description: 'A thief who steals corporate secrets through the use of dream-sharing technology.',
-        releaseDate: new Date('2010-07-16'),
-        director: 'Christopher Nolan',
-        genre: 'Sci-Fi',
-      },
-      {
-        title: 'The Godfather',
-        description: 'The aging patriarch of an organized crime dynasty transfers control to his reluctant son.',
-        releaseDate: new Date('1972-03-24'),
-        director: 'Francis Ford Coppola',
-        genre: 'Crime',
-      },
-      {
-        title: 'Spirited Away',
-        releaseDate: new Date('2001-07-20'),
-        director: 'Hayao Miyazaki',
-        genre: 'Fantasy',
-      },
-      {
-        title: 'Parasite',
-        description: 'Greed and class discrimination threaten the newly formed symbiotic relationship.',
-        releaseDate: new Date('2019-05-30'),
-        director: 'Bong Joon-ho',
-        genre: 'Thriller',
-      },
-      {
-        title: 'Interstellar',
-        description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-        releaseDate: new Date('2014-11-07'),
-        director: 'Christopher Nolan',
-        genre: 'Sci-Fi',
-      },
-    ],
-  });
-
-  console.log(createMovies, 'Create user response');
+  console.log(newUser);
 }
 
 main();
