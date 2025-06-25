9f5d8a9ade270e40a972ab892988934fb5fea13c
tariqul420 <tariqul.developer@gmail.com>
2025-06-25 22:41:29 +0600

    feat: enhance find script with detailed logging for user and movie retrieval

--- src/find.ts
+++ src/find.ts
@@ -3,9 +3,36 @@ import { PrismaClient } from '../generated/prisma';
 const prisma = new PrismaClient();
 
 async function main() {
-  // find all movies
+  // find all
   const findMovies = await prisma.movie.findMany();
-  console.log(findMovies);
+  console.log(findMovies, 'Find all movies.');
+
+  // find unique
+  const findUser = await prisma.user.findUnique({
+    where: {
+      email: 'tariqul.dev7@gmail.com',
+    },
+  });
+
+  console.log(findUser, 'Find a user.');
+
+  // find first
+  const findFirst = await prisma.user.findFirst({
+    where: {
+      password: '123456',
+    },
+  });
+
+  console.log(findFirst, 'Find First.');
+
+  // find a throw error
+  const findAndError = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: 'tariqul.dev7@gmail.com',
+    },
+  });
+
+  console.log(findAndError, 'Find a user and throw error');
 }
 
 main();
